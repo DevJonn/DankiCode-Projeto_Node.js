218098705156a32248efa89f8ad52709fe535a04
Jonathan Torres <jonathanlt68@gmail.com>
2023-11-09 13:43:17 -0300

    Update
    Começando a Criar Rotas, instalando o nodemon e colocando em ação

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+
+    res.send ('Carregado página inicial.');
+
+});
+
 app.listen(5000,()=>{
     console.log('server rodando!');
 })
