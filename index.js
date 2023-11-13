e44320adf12e6aaf98d50db2e9b3b985db6162d4
Jonathan Torres <jonathanlt68@gmail.com>
2023-11-13 11:46:07 -0300

    update index
    Criando rotas, pastas e funções com o express

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express();
 
 //Aula Criando Rotas iniciais
 
+app.engine('html', require('ejs'.renderFile)); 
+//setar a engine para renderização do tipo html usando o ejs para renderizar
+app.set('view engine', 'html');
+//aqui estamos setando a view engine para ser html
+app.use('public', express.static(path.join(__dirname, 'public')));
+//informar que o diretorio estático onde fica arquivos fotos e css está na pasta public
+app.set('views', path.join(__dirname, '/views'));
+//informando a pasta views usando a função path.join e função __dirname
+
 app.get('/',(req,res)=>{
 
-    res.send ('Carregado página inicial.');
 
 });
 
